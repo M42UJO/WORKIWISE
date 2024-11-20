b56c28248ada782d169a91126753d7438be5aa77
M42UJO <khamso0092@gmail.com>
2024-11-21 04:46:35 +0700

    a

--- src/pages/login/components/LoginLogo.tsx
+++ src/pages/login/components/LoginLogo.tsx
@@ -3,7 +3,7 @@ import logoWorkiwise from "../../../assets/img/logoWorkiwise.png";
 function LoginLogo() {
     return (
         <>
-            <div className="flex items-center space-x-2">
+            <div className="flex items-center">
                 {/* Logo Icon */}
                 <img src={logoWorkiwise} alt="logo" className="w-72" />
             </div>
