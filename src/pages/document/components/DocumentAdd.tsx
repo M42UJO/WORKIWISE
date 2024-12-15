fa6d57ba7fe43be1902ae8a4147f65ee0700e609
M42UJO <khamso0092@gmail.com>
2024-12-15 14:41:57 +0700

    upload file

--- src/pages/document/components/DocumentAdd.tsx
+++ src/pages/document/components/DocumentAdd.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import hljs from "highlight.js";
 import "highlight.js/styles/monokai.css";
-import Quill from "quill"; // Import Quill.js
+import Quill from "quill"; 
 import "quill/dist/quill.snow.css";
 
 // Add Syntax Module
@@ -11,19 +11,17 @@ import QuillSyntax from "quill/modules/syntax";
 Quill.register("modules/syntax", QuillSyntax);
 
 const QuillEditor: React.FC = () => {
-  const [content, setContent] = useState<string>("");
 
-  // Define Modules
   const modules = {
     toolbar: [
       [{ header: [1, 2, 3, false] }],
-      ["bold", "italic", "underline", "strike"], // Text styling
-      [{ list: "ordered" }, { list: "bullet" }], // Lists
-      [{ script: "sub" }, { script: "super" }], // Subscript/Superscript
-      [{ indent: "-1" }, { indent: "+1" }], // Indent
-      [{ align: [] }], // Text alignment
-      ["link", "image", "code-block"], // Links, images, and code blocks
-      ["clean"], // Remove formatting
+      ["bold", "italic", "underline", "strike"], 
+      [{ list: "ordered" }, { list: "bullet" }], 
+      [{ script: "sub" }, { script: "super" }], 
+      [{ indent: "-1" }, { indent: "+1" }], 
+      [{ align: [] }], 
+      ["link", "image", "code-block"], 
+      ["clean"], 
     ],
     syntax: {
       highlight: (text: string) => hljs.highlightAuto(text).value,
@@ -46,16 +44,11 @@ const QuillEditor: React.FC = () => {
     "code-block",
   ];
 
-  const handleContentChange = (value: string) => {
-    setContent(value);
-  };
 
   return (
     <div className="flex ">
       <div className="w-full ">
         <ReactQuill
-          value={content}
-          // onChange={handleContentChange}
           modules={modules}
           formats={formats}
           theme="snow"
